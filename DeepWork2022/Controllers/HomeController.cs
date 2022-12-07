cafcd6a84b19cb195f2cfc8d1d60abc7c897e624
sheyo <sheyo@DESKTOP-QVRND3R>
2022-12-07 14:59:48 +0530

    Add project files.

---
+++ DeepWork2022/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeepWork2022.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() //Action Method 
+        {
+            return View();
+        }
+
+        public IActionResult About() 
+        {
+            return View();
+        }
+
+
+    }
+}
